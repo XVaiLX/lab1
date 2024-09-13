6dc78a748bcda1cd25db76386ff3deb2a28b8d8a
Dima <dadaadada@yandex.ru>
2024-09-13 14:16:33 +0300

    N 2

--- LAB1/LAB1.cpp
+++ LAB1/LAB1.cpp
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-	int M[10]={1, 2, 3, 4, 5, 6, 7, 8, 9, 0}, i, max=0,min=0;
+	int M[10], i, max=0,min=0;
 	for (i = 0; i < 10; i++) {
+		M[i] = rand();
 		if (max <= M[i])
 			max = M[i];
 		if (min >= M[i])
